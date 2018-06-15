083e48ff56d7f2924d827cf6d6708420227575f4
nrontsis <nrontsis@gmail.com>
2018-06-15 15:59:08 +0100

    #2 Add test for sparse GPs

---
+++ tests/test_sparse_predictions.py
@@ -0,0 +1,70 @@
+from pilco.models import SMGPR
+import numpy as np
+import os
+from gpflow import autoflow
+from gpflow import settings
+import oct2py
+octave = oct2py.Oct2Py()
+dir_path = os.path.dirname(os.path.realpath("__file__")) + "/tests/Matlab Code"
+octave.addpath(dir_path)
+
+float_type = settings.dtypes.float_type
+
+@autoflow((float_type,[None, None]), (float_type,[None, None]))
+def predict_wrapper(smgpr, m, s):
+    return smgpr.predict_on_noisy_inputs(m, s)
+
+@autoflow()
+def get_induced_points(smgpr):
+    return smgpr.Z
+
+def test_predictions():
+    np.random.seed(0)
+    d = 3  # Input dimension
+    k = 2  # Number of outputs
+
+    # Training Dataset
+    X0 = np.random.rand(100, d)
+    A = np.random.rand(d, k)
+    Y0 = np.sin(X0).dot(A) + 1e-3*(np.random.rand(100, k) - 0.5)  #  Just something smooth
+    smgpr = SMGPR(X0, Y0, num_induced_points=30)
+
+    smgpr.optimize()
+
+    # Generate input
+    m = np.random.rand(1, d)  # But MATLAB defines it as m'
+    s = np.random.rand(d, d)
+    s = s.dot(s.T)  # Make s positive semidefinite
+
+    M, S, V = predict_wrapper(smgpr, m, s)
+
+    # convert data to the struct expected by the MATLAB implementation
+    lengthscales = np.stack([model.kern.lengthscales.value for model in smgpr.models])
+    variance = np.stack([model.kern.variance.value for model in smgpr.models])
+    noise = np.stack([model.likelihood.variance.value for model in smgpr.models])
+
+    hyp = np.log(np.hstack(
+        (lengthscales,
+         np.sqrt(variance[:, None]),
+         np.sqrt(noise[:, None]))
+    )).T
+
+    gpmodel = oct2py.io.Struct()
+    gpmodel.hyp = hyp
+    gpmodel.inputs = X0
+    gpmodel.targets = Y0
+    gpmodel.induce = get_induced_points(smgpr)
+
+    # Call gp0 in octave
+    M_mat, S_mat, V_mat = octave.gp1(gpmodel, m.T, s, nout=3)
+
+    assert M.shape == M_mat.T.shape
+    assert S.shape == S_mat.shape
+    assert V.shape == V_mat.shape
+    np.testing.assert_allclose(M, M_mat.T, rtol=1e-4)
+    np.testing.assert_allclose(S, S_mat, rtol=1e-4)
+    np.testing.assert_allclose(V, V_mat, rtol=1e-4)
+
+
+if __name__ == '__main__':
+    test_predictions()
