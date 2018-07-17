57b2aa770455a181f7ced75a68c964c2cb2accce
kyr-pol <kyriakostp@hotmail.com>
2018-07-17 21:00:08 +0100

    User selected parameters for the reward function are passed to the reward function constructor and not PILCO

--- examples/inverted_pendulum.py
+++ examples/inverted_pendulum.py
@@ -2,6 +2,7 @@ import numpy as np
 import gym
 from pilco.models import PILCO
 from pilco.controllers import RbfController, LinearController
+from pilco.rewards import ExponentialReward
 np.random.seed(0)
 
 env = gym.make('InvertedPendulum-v2')
@@ -40,8 +41,9 @@ controller = RbfController(state_dim=state_dim, control_dim=control_dim, num_bas
 #controller = LinearController(state_dim=state_dim, control_dim=control_dim)
 
 pilco = PILCO(X, Y, controller=controller, horizon=40)
-# Example of reward function input 
-# pilco = PILCO(X, Y, controller=controller, horizon=40, reward_t=np.array([0.1,0,0,0]))
+# Example of user provided reward function, setting a custom target state
+# R = ExponentialReward(state_dim=state_dim, t=np.array([0.1,0,0,0]))
+# pilco = PILCO(X, Y, controller=controller, horizon=40, reward=R)
 
 # Example of fixing a parameter, optional, for a linear controller only
 #pilco.controller.b = np.array([[0.0]])
