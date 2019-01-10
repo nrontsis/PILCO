95161e88c7eb23441540791a219eda20854dc060
kyr-pol <kyriakostp@hotmail.com>
2019-01-10 18:28:19 +0000

    Cleaned up files created from test_load_save.py, updated .gitignore

--- tests/test_load_save.py
+++ tests/test_load_save.py
@@ -21,15 +21,19 @@ def test_load_save():
     Y0 = np.sin(X0).dot(A) + 1e-3*(np.random.rand(100, d) - 0.5)  #  Just something smooth
     pilco = PILCO(X0, Y0)
     pilco.optimize()
-    save_pilco("", X0, Y0, pilco)
+    try:
+        os.mkdir("tmp/")
+    except FileExistsError:
+        pass
+    save_pilco("tmp/", X0, Y0, pilco)
 
-    pilco2 = load_pilco("")
+    pilco2 = load_pilco("tmp/")
 
     pilco_sp = PILCO(X0, Y0, num_induced_points=15)
     pilco_sp.optimize()
-    save_pilco("sparse_", X0, Y0, pilco_sp, sparse=True)
+    save_pilco("tmp/sparse_", X0, Y0, pilco_sp, sparse=True)
 
-    pilco_sp2 = load_pilco("sparse_", sparse=True)
+    pilco_sp2 = load_pilco("tmp/sparse_", sparse=True)
 
     m = np.random.rand(1, d)  # But MATLAB defines it as m'
     s = np.random.rand(d, d)
@@ -47,6 +51,11 @@ def test_load_save():
     np.testing.assert_allclose(S1, S2, rtol=1e-6)
     np.testing.assert_allclose(r1, r2, rtol=1e-6)
 
+    filenames = ["X.csv", "Y.csv", "pilco_values.npy", "model_0.npy", "model_1.npy"]
+    filenames = filenames + ["sparse_" + x for x in filenames] + ["sparse_n_ind.txt"]
+    print(filenames)
+    for f in filenames:
+        os.remove("tmp/" + f)
 
 if __name__=='__main__':
     test_load_save()
