Fixed rbf controller bug. Mountain car and inverted pendulum run successfully with occasional numerical errors

pilco/controllers.py
@@ -67,8 +67,9 @@ class LinearController(gpflow.Module):
 class FakeGPR(gpflow.Module):
     def __init__(self, data, kernel):
         gpflow.Module.__init__(self)
-        self.X = Parameter(data[0])
-        self.Y = Parameter(data[1])
+        self.X = Parameter(data[0], name="DataX", dtype=gpflow.default_float())
+        self.Y = Parameter(data[1], name="DataY", dtype=gpflow.default_float())
+        self.data = [self.X, self.Y]
         self.kernel = kernel
         self.likelihood = gpflow.likelihoods.Gaussian()
 
@@ -92,7 +93,7 @@ class RbfController(MGPR):
         self.models = []
         for i in range(self.num_outputs):
             kernel = gpflow.kernels.SquaredExponential(lengthscales=tf.ones([data[0].shape[1],], dtype=float_type))
-            self.models.append(FakeGPR(data, kernel))
+            self.models.append(FakeGPR((data[0], data[1][:,i:i+1]), kernel))
 
     def compute_action(self, m, s, squash=True):
         '''

pilco/models/mgpr.py
@@ -20,7 +20,6 @@ class MGPR(gpflow.Module):
     def __init__(self, data, name=None):
         super(MGPR, self).__init__(name)
 
-        self.data = data
         self.num_outputs = data[1].shape[1]
         self.num_dims = data[0].shape[1]
         self.num_datapoints = data[0].shape[0]
@@ -33,16 +32,19 @@ class MGPR(gpflow.Module):
         for i in range(self.num_outputs):
             kern = gpflow.kernels.SquaredExponential(lengthscales=tf.ones([data[0].shape[1],], dtype=float_type))
             #TODO: Maybe fix noise for better conditioning
-            #model.kernel.lengthscales.prior = tfd.Gamma(f64(1.0), f64(1.0))
-            kern.lengthscales.prior = tfd.Gamma(f64(1.0),f64(10.0)) # priors have to be included before
+            kern.lengthscales.prior = tfd.Gamma(f64(2.0),f64(10.0)) # priors have to be included before
             kern.variance.prior = tfd.Gamma(f64(1.5),f64(2.0))    # before the model gets compiled
             self.models.append(gpflow.models.GPR((data[0], data[1][:, i:i+1]), kernel=kern))
-            # self.models[i].clear(); self.models[i].compile()
+            self.models[-1].likelihood.prior = tfd.Gamma(f64(1.2),f64(0.1))
 
     def set_data(self, data):
-        self.data = data
         for i in range(len(self.models)):
-            self.models[i].data = (data[0], data[1][:, i:i+1])
+            if isinstance(self.models[i].data[0], gpflow.Parameter):
+                self.models[i].X.assign(data[0])
+                self.models[i].Y.assign(data[1][:, i:i+1])
+                self.models[i].data = [self.models[i].X, self.models[i].Y]
+            else:
+                self.models[i].data = (data[0], data[1][:, i:i+1])
 
     def optimize(self, restarts=1):
         if len(self.optimizers) == 0:  # This is the first call to optimize();
@@ -165,13 +167,13 @@ class MGPR(gpflow.Module):
     @property
     def Y(self):
         return tf.concat(
-            [model.Y.parameter_tensor for model in self.models],
+            [model.data[1] for model in self.models],
             axis = 1
         )
 
     @property
     def X(self):
-        return self.models[0].X.parameter_tensor
+        return self.models[0].data[0]
 
     @property
     def lengthscales(self):
@@ -192,9 +194,5 @@ class MGPR(gpflow.Module):
         )
 
     @property
-    def X(self):
-        return self.data[0]
-
-    @property
-    def Y(self):
-        return self.data[1]
+    def data(self):
+        return (self.X, self.Y)

pilco/utils.py
@@ -1,7 +1,6 @@
 import numpy as np
-from gpflow import autoflow
-from gpflow import settings
-float_type = settings.dtypes.float_type
+from gpflow import config
+float_type = config.default_float()
 
 
 def rollout(env, pilco, timesteps, verbose=True, random=False, SUBS=1, render=False):
@@ -36,21 +35,17 @@ def policy(env, pilco, x, random):
         return pilco.compute_action(x[None, :])[0, :]
 
 
-@autoflow((float_type,[None, None]), (float_type,[None, None]))
 def predict_one_step_wrapper(mgpr, m, s):
     return mgpr.predict_on_noisy_inputs(m, s)
 
 
-@autoflow((float_type,[None, None]), (float_type,[None, None]), (np.int32, []))
 def predict_trajectory_wrapper(pilco, m, s, horizon):
     return pilco.predict(m, s, horizon)
 
 
-@autoflow((float_type,[None, None]), (float_type,[None, None]))
 def compute_action_wrapper(pilco, m, s):
     return pilco.controller.compute_action(m, s)
 
 
-@autoflow((float_type, [None, None]), (float_type, [None, None]))
 def reward_wrapper(reward, m, s):
     return reward.compute_reward(m, s)