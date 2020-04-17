Updated inverted pendulum and pendulum swingup examples. Fixed noise variance for RBF controllers. Modified mgpr priors

examples/inverted_pendulum.py
@@ -4,6 +4,7 @@ from pilco.models import PILCO
 from pilco.controllers import RbfController, LinearController
 from pilco.rewards import ExponentialReward
 import tensorflow as tf
+from gpflow import set_trainable
 # from tensorflow import logging
 np.random.seed(0)
 
@@ -12,7 +13,7 @@ from pilco.utils import rollout, policy
 env = gym.make('InvertedPendulum-v2')
 # Initial random rollouts to generate a dataset
 X,Y, _, _ = rollout(env=env, pilco=None, random=True, timesteps=40, render=True)
-for i in range(1,3):
+for i in range(1,5):
     X_, Y_, _, _ = rollout(env=env, pilco=None, random=True,  timesteps=40, render=True)
     X = np.vstack((X, X_))
     Y = np.vstack((Y, Y_))
@@ -20,7 +21,7 @@ for i in range(1,3):
 
 state_dim = Y.shape[1]
 control_dim = X.shape[1] - state_dim
-controller = RbfController(state_dim=state_dim, control_dim=control_dim, num_basis_functions=5)
+controller = RbfController(state_dim=state_dim, control_dim=control_dim, num_basis_functions=10)
 # controller = LinearController(state_dim=state_dim, control_dim=control_dim)
 
 pilco = PILCO((X, Y), controller=controller, horizon=40)
@@ -28,16 +29,11 @@ pilco = PILCO((X, Y), controller=controller, horizon=40)
 # R = ExponentialReward(state_dim=state_dim, t=np.array([0.1,0,0,0]))
 # pilco = PILCO(X, Y, controller=controller, horizon=40, reward=R)
 
-# Example of fixing a parameter, optional, for a linear controller only
-# pilco.controller.b = np.array([[0.0]])
-# pilco.controller.b.trainable = False
-
 for rollouts in range(3):
     pilco.optimize_models()
     pilco.optimize_policy()
     import pdb; pdb.set_trace()
     X_new, Y_new, _, _ = rollout(env=env, pilco=pilco, timesteps=100, render=True)
-    #print("No of ops:", len(tf.get_default_graph().get_operations()))
     # Update dataset
     X = np.vstack((X, X_new)); Y = np.vstack((Y, Y_new))
     pilco.mgpr.set_data((X, Y))

pilco/models/mgpr.py
@@ -32,10 +32,10 @@ class MGPR(gpflow.Module):
         for i in range(self.num_outputs):
             kern = gpflow.kernels.SquaredExponential(lengthscales=tf.ones([data[0].shape[1],], dtype=float_type))
             #TODO: Maybe fix noise for better conditioning
-            kern.lengthscales.prior = tfd.Gamma(f64(2.0),f64(10.0)) # priors have to be included before
-            kern.variance.prior = tfd.Gamma(f64(1.5),f64(2.0))    # before the model gets compiled
+            kern.lengthscales.prior = tfd.Gamma(f64(2.0),f64(1/4)) # priors have to be included before
+            kern.variance.prior = tfd.Gamma(f64(2.0),f64(1/0.4))    # before the model gets compiled
             self.models.append(gpflow.models.GPR((data[0], data[1][:, i:i+1]), kernel=kern))
-            self.models[-1].likelihood.prior = tfd.Gamma(f64(1.2),f64(0.1))
+            self.models[-1].likelihood.prior = tfd.Gamma(f64(1.2),f64(1/0.05))
 
     def set_data(self, data):
         for i in range(len(self.models)):